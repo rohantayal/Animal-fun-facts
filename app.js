f2017b09f980f8a87dd37ea3d34881307e8758ea
Rohan Tayal <109153434+rohantayal@users.noreply.github.com>
2023-06-24 10:08:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { animals } from "./animals";
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-import Contact from './Contact'
+const container = document.getElementById("app");
+const root = createRoot(container);
 
-function App() {
-  return <Contact />;
+const title = "";
+
+const background = (
+  <img className="background" alt="ocean" src="/images/ocean.jpg" />
+);
+
+function displayFact(e) {
+  const animal= e.target.alt;
+  const index=Math.floor(Math.random() * animals[animal].facts.length);
+  const funFact = animals[animal].facts[index];
+  const p=document.getElementById('fact');
+  p.innerHTML=funFact;
 }
 
-export default App
+let images = [];
+for (const animal in animals) {
+  images.push(
+    <img
+      onClick= {displayFact}
+      key={animal}
+      className='animal'
+      alt={animal}
+      src={animals[animal].image}
+      aria-label={animal}
+      role="button"
+    />
+  );
+}
+
+const animalFacts = (
+  <div>
+    <h1>{title === "" ? "Click an animal for a fun fact" : title}</h1>
+    {background}
+    <p id='fact'></p>
+    <div className='animals'>
+      {images}
+    </div>
+  </div>
+);
+root.render(animalFacts);
+
+
